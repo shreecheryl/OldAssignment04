9418bb561e6cff1ecbcbf282e1fe7313ec4cbdcc
Cheryl Davis <shreecheryl@abs-macbook-air.attlocal.net>
2016-07-04 12:33:38 -0700

    completed

--- scripts.js
+++ scripts.js
@@ -58,20 +58,19 @@ Animal.prototype.speak = function() {
 window.console.log(kuber.speak());
 */
 // STEP 9
-
+/*
  var Animal = function() {
     var type = "cat";
     var breed = "Maine Coon";
     var color = "black";
     var height = "14 in";
     var length = "20 in";
-    this.checkType = function() {
+    this.checkType = function() {   // not happy with this since it is privileged not private
         if (type == "dog") {
-            var _type =  "dog";
+            return "dog";
         } else {
-            var _type = "cat";
+            return "cat";
         }
-    return _type;
     }
 }
 Animal.prototype.speak = function() {
@@ -79,6 +78,69 @@ Animal.prototype.speak = function() {
 }
 var kuber = new Animal();
 kuber.speak();
+*/
+// STEP 10
+/*
+String.prototype.findWords = function() {
+    this.text = text.toString().toLowerCase();
+    window.console.log(this.text);
+    var word = prompt("What word are you looking for?").toLowerCase();
+    var patt1 = new RegExp(word, "g");
+    var matches = this.text.match(patt1);
+    if (matches === null) {
+        window.console.log("The word \'" + word + "\' is not in this paragraph.")
+    } else {
+        window.console.log("The word \'" + word + "\' is in this paragraph " + matches.length + " times.");
+    }
+}
+
+var text = "My dog is better than all of the other dogs at the Fiesta Island Dog Park.";
+text.findWords();
+*/
+// THE RECIPE CARD
+/*
+var Recipe = function() {
+    this._title = arguments[0];
+    this._servings = arguments[1];
+    this._ingredients = [];
+    var i = 2;
+    for (i; i < arguments.length; i++) {
+        this._ingredients[i - 2] = arguments[i];
+    }
+    window.console.log(this._ingredients);
+};
+Recipe.prototype.displayInfo = function() {
+    window.console.log(this._title + "\nServes: " + this._servings + "\nIngredients:\n");
+    for (var item in this._ingredients) {
+        window.console.log("- " + this._ingredients[item] + "\n");
+    }
+}
+var gaucamole = new Recipe("Guacamole", 4, "3 Avocados", "1 Lime", "1 tsp Salt", "1/2 cup Onion", "3 Tbls Cilantro", "2 Diced Tomatoes", "1 tsp Garlic", "1 pinch Ground Pepper")
+gaucamole.displayInfo();
+*/
+// THE READING LIST
+/*
+var Book = function(title, author, alreadyRead) {
+    this._title = title;
+    this._author = author;
+    this._alreadyRead = alreadyRead;
+}
+var book1 = new Book("Finding Your Way in a Wild New World", "Martha Beck", false);
+var book2 = new Book("Spiritually Incorrect Enlightenment", "Jed McKenna", true);
+var book3 = new Book("Spiritual Warfare", "Jed McKenna", true);
+var book4 = new Book("Zen and the Art of Falling in Love", "Brenda Shoshanna", true);
+var book5 = new Book("A Thousand Names for Joy", "Byron Katie", true);
+var bookLog = [book1, book2, book3, book4, book5];
+for (i in bookLog) {
+    window.console.log(bookLog[i]._title + " by " + bookLog[i]._author);
+    if (bookLog[i]._alreadyRead === true) {
+        window.console.log("You already read " + bookLog[i]._title + " by " + bookLog[i]._author);
+    } else {
+        window.console.log("You still need to read " + bookLog[i]._title + " by " + bookLog[i]._author);
+    }
+}
+*/
+
 
 
 
